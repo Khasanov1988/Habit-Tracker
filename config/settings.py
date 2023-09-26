530c4dee0859add7544c36407eedfb5cf812e6e8
Ilnar Khasanov <Khasanov1988@gmail.com>
2023-09-26 15:35:52 +0200

    Settings.py is fixed

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ load_dotenv(dotenv_path=dot_env)
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jrum_gj9#rmx^z=qey2_u=9a7*vo3q9b@d7s%rge2fs#esf@%a'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
